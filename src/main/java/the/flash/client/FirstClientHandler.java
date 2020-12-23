265aa03307bfc9d5d2f93deedf2d980165778ea9
junbo.chen <timmydargon@sina.com>
2020-12-23 20:54:38 +0800

    监听流程梳理

--- src/main/java/the/flash/client/FirstClientHandler.java
+++ src/main/java/the/flash/client/FirstClientHandler.java
@@ -15,7 +15,7 @@ import java.util.Date;
 public class FirstClientHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelActive(ChannelHandlerContext ctx) {
-        System.out.println(new Date() + ": 客户端写出数据");
+        System.out.println(new Date() + ": channelActive客户端写出数据");
 
         // 1.获取数据
         ByteBuf buffer = getByteBuf(ctx);
@@ -34,11 +34,48 @@ public class FirstClientHandler extends ChannelInboundHandlerAdapter {
         return buffer;
     }
 
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(new Date() + "channelRegistered");
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(new Date() + "channelUnregistered");
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(new Date() + "channelInactive");
+    }
+
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(new Date() + "channelReadComplete");
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        System.out.println(new Date() + "userEventTriggered");
+    }
+
+    @Override
+    public void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(new Date() + "channelWritabilityChanged");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
+            throws Exception {
+        System.out.println(new Date() + "exceptionCaught");
+    }
+
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         ByteBuf byteBuf = (ByteBuf) msg;
 
-        System.out.println(new Date() + ": 客户端读到数据 -> " + byteBuf.toString(Charset.forName("utf-8")));
+        System.out.println(new Date() + ": channelRead客户端读到数据 -> " + byteBuf.toString(Charset.forName("utf-8")));
     }
 }
--- src/main/java/the/flash/server/FirstServerHandler.java
+++ src/main/java/the/flash/server/FirstServerHandler.java
@@ -18,10 +18,10 @@ public class FirstServerHandler extends ChannelInboundHandlerAdapter {
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         ByteBuf byteBuf = (ByteBuf) msg;
 
-        System.out.println(new Date() + ": 服务端读到数据 -> " + byteBuf.toString(Charset.forName("utf-8")));
+        System.out.println(new Date() + ": channelRead服务端读到数据 -> " + byteBuf.toString(Charset.forName("utf-8")));
 
         // 回复数据到客户端
-        System.out.println(new Date() + ": 服务端写出数据");
+        System.out.println(new Date() + ": channelRead服务端写出数据");
         ByteBuf out = getByteBuf(ctx);
         ctx.channel().writeAndFlush(out);
     }
@@ -34,5 +34,47 @@ public class FirstServerHandler extends ChannelInboundHandlerAdapter {
         buffer.writeBytes(bytes);
 
         return buffer;
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(new Date() + "channelRegistered");
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(new Date() + "channelUnregistered");
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(new Date() + "channelActive");
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(new Date() + "channelInactive");
+    }
+
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(new Date() + "channelReadComplete");
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        System.out.println(new Date() + "userEventTriggered");
+    }
+
+    @Override
+    public void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(new Date() + "channelWritabilityChanged");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
+            throws Exception {
+        System.out.println(new Date() + "exceptionCaught");
     }
 }
